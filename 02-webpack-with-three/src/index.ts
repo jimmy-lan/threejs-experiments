c8ca4ebbc6e9cc022a900848a02f5721175eb2ff
Jimmy <jimmylanbcn@gmail.com>
2021-11-16 06:31:24 -0500

    Add main function

--- 02-webpack-with-three/src/index.ts
+++ 02-webpack-with-three/src/index.ts
@@ -1,3 +1,14 @@
 import "./style.css";
+import { getSphere } from "./sphere";
+import { getCamera } from "./camera";
+import { Size } from "./size";
+import { getRenderer } from "./renderer";
 
-console.log("Hello world!");
+const start = () => {
+  const size = new Size();
+  const sphere = getSphere();
+  const camera = getCamera(size);
+  const renderer = getRenderer(size);
+};
+
+start();
