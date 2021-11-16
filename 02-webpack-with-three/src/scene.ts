01bc4c8e85d414911e666b3ad433b09d936f5ea5
Jimmy <jimmylanbcn@gmail.com>
2021-11-16 06:58:49 -0500

    Use axes helper

--- 02-webpack-with-three/src/scene.ts
+++ 02-webpack-with-three/src/scene.ts
@@ -1,12 +1,15 @@
 import { Camera, Scene } from "three";
 import { getSphere } from "./sphere";
+import { getAxesHelper } from "./axesHelper";
 
 export const constructScene = (camera: Camera) => {
   const scene = new Scene();
   const sphere = getSphere();
+  const axesHelper = getAxesHelper();
   sphere.position.set(1, 1, sphere.position.z);
   camera.position.z = 8;
   scene.add(sphere);
   scene.add(camera);
+  scene.add(axesHelper);
   return scene;
 };
