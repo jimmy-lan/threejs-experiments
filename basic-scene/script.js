64c60c8417469c0092be24591f47c020fe92faa0
Jimmy <jimmylanbcn@gmail.com>
2021-11-15 21:37:04 -0500

    Create scene

--- basic-scene/script.js
+++ basic-scene/script.js
@@ -1,3 +1,11 @@
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#6898FD" });
 const box = new THREE.Mesh(geometry, material);
+
+const scene = THREE.Scene();
+scene.add(box);
+
+const renderSize = {
+  width: 600,
+  height: 600,
+};
