4824d1547fb70731566b6ea324bdaca13d14cd04
Jimmy <jimmylanbcn@gmail.com>
2021-11-16 07:09:55 -0500

    Add scene object transformation logic

--- 02-webpack-with-three/src/scene.ts
+++ 02-webpack-with-three/src/scene.ts
@@ -7,7 +7,15 @@ export const constructScene = (camera: Camera) => {
   const sphere = getSphere();
   const axesHelper = getAxesHelper();
   sphere.position.set(1, 1, sphere.position.z);
-  camera.position.z = 8;
+  sphere.scale.set(0.5, 0.5, 2);
+  /*
+   * When rotating objects, imagine an axes helper rotating with the 3D object.
+   * So, the order of rotation matters.
+   */
+  sphere.rotation.x = Math.PI * 0.25;
+  sphere.rotation.y = Math.PI * 0.25;
+  // sphere.rotation.reorder("YX")
+  camera.position.set(2, 2, 8);
   scene.add(sphere);
   scene.add(camera);
   scene.add(axesHelper);
