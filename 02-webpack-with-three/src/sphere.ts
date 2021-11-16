0e9e2ce79846b89c5956e60e8c99b92c6ab35f52
Jimmy <jimmylanbcn@gmail.com>
2021-11-16 06:42:16 -0500

    Change sphere color

--- 02-webpack-with-three/src/sphere.ts
+++ 02-webpack-with-three/src/sphere.ts
@@ -2,6 +2,6 @@ import { Mesh, MeshBasicMaterial, SphereGeometry } from "three";
 
 export const getSphere = () => {
   const geometry = new SphereGeometry(2, 16, 16);
-  const material = new MeshBasicMaterial({ color: "#ffff00" });
+  const material = new MeshBasicMaterial({ color: "#6898FD" });
   return new Mesh(geometry, material);
 };
