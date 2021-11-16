92088febe818b0e147f0471e264e3aa15f415aae
Jimmy <jimmylanbcn@gmail.com>
2021-11-15 21:47:50 -0500

    Refactor code

--- basic-scene/script.js
+++ basic-scene/script.js
@@ -1,14 +1,28 @@
+const getCanvas = () => {
+  const canvas = document.querySelector("canvas.root");
+  if (!canvas) {
+    console.error("Unable to locate root canvas to render object.");
+  }
+  return canvas;
+};
+
+class Size {
+  constructor(width = 600, height = 600) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getAspect() {
+    return this.width / this.height;
+  }
+}
+
+const canvas = getCanvas();
+const renderSize = new Size();
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "#6898FD" });
 const box = new THREE.Mesh(geometry, material);
-
-const renderSize = {
-  width: 600,
-  height: 600,
-  getAspect: function () {
-    return this.width / this.height;
-  },
-};
 
 const camera = new THREE.PerspectiveCamera(50, renderSize.getAspect());
 
