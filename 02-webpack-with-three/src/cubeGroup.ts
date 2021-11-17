758dda97652304445c67a830f5058e7e96b19f42
Jimmy <jimmylanbcn@gmail.com>
2021-11-16 20:08:43 -0500

    Construct three cubes in the group

--- 02-webpack-with-three/src/cubeGroup.ts
+++ 02-webpack-with-three/src/cubeGroup.ts
@@ -1,7 +1,30 @@
-import { Group } from "three";
+import {
+  BoxGeometry,
+  ColorRepresentation,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+} from "three";
+
+const getCube = (
+  width: number,
+  height: number,
+  depth: number,
+  color: ColorRepresentation
+) => {
+  return new Mesh(
+    new BoxGeometry(width, height, depth),
+    new MeshBasicMaterial({ color })
+  );
+};
 
 const getCubeGroup = () => {
   const group = new Group();
-  // TODO
+  const cube1 = getCube(1, 1, 1, "#ff0000");
+  const cube2 = getCube(1, 1, 1, "#00ff00");
+  cube2.position.x = -2;
+  const cube3 = getCube(1, 1, 1, "#0000ff");
+  cube3.position.x = 2;
+  group.add(cube1, cube2, cube3);
   return group;
 };
