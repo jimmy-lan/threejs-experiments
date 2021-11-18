e7ba037c9863a26101d09378583c0cfe557b50a9
Jimmy <jimmylanbcn@gmail.com>
2021-11-17 21:51:12 -0500

    Create 04-camera folder

---
+++ 04-cameras/src/index.ts
@@ -0,0 +1,27 @@
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import "./style.css";
+import { Size } from "./size";
+
+const RENDER_CANVAS_SELECTOR = "canvas.root";
+
+const size = new Size();
+const cube = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({ color: "#6898FD" })
+);
+const camera = new PerspectiveCamera(50, size.aspect);
+camera.position.z = 3;
+
+const scene = new Scene();
+scene.add(cube, camera);
+const canvas = document.querySelector(RENDER_CANVAS_SELECTOR);
+const renderer = new WebGLRenderer({ canvas });
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera);
