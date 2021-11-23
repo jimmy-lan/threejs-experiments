7af55665b0504784aec038f4aea542c10cdea23c
Jimmy <jimmylanbcn@gmail.com>
2021-11-22 19:05:26 -0500

    Create 05-full-screen folder

---
+++ 05-full-screen/src/index.ts
@@ -0,0 +1,66 @@
+import {
+  BoxGeometry,
+  EdgesGeometry,
+  LineBasicMaterial,
+  LineSegments,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  Vector2,
+  WebGLRenderer,
+} from "three";
+import "./style.css";
+import { Size } from "./size";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const RENDER_CANVAS_SELECTOR = "canvas.root";
+
+const size = new Size();
+const cube = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({ color: "#6898FD" })
+);
+const cubeEdgeLines = new LineSegments(
+  new EdgesGeometry(cube.geometry),
+  new LineBasicMaterial({ color: "#fff", linewidth: 3, linejoin: "bevel" })
+);
+const camera = new PerspectiveCamera(50, size.aspect);
+camera.position.z = 3;
+
+const scene = new Scene();
+scene.add(cube, cubeEdgeLines, camera);
+
+const canvas = document.querySelector(RENDER_CANVAS_SELECTOR);
+const controls = new OrbitControls(camera, canvas as HTMLElement);
+// https://threejs.org/docs/#examples/en/controls/OrbitControls.dampingFactor
+controls.enableDamping = true;
+// controls.target.y = 1;
+// controls.update();
+
+const renderer = new WebGLRenderer({ canvas });
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera);
+
+const cursor = new Vector2(0, 0);
+addEventListener("mousemove", (event: MouseEvent) => {
+  // x, y range: [-0.5, 0.5]
+  cursor.x = event.clientX / size.width - 0.5;
+  cursor.y = -(event.clientY / size.height - 0.5);
+});
+
+const renderFrame = () => {
+  // camera.position.set(
+  //   Math.sin(cursor.x * Math.PI * 2) * 3,
+  //   cursor.y * 5,
+  //   Math.cos(cursor.x * Math.PI * 2) * 3
+  // );
+  // camera.lookAt(cube.position);
+
+  controls.update();
+
+  renderer.render(scene, camera);
+  requestAnimationFrame(renderFrame);
+};
+
+requestAnimationFrame(renderFrame);
