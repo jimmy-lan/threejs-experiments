c7335669afee65fdfc0a8883b140f429757014a5
Jimmy <jimmylanbcn@gmail.com>
2021-11-24 19:58:58 -0500

    Fix bugs

--- 06-geometry-and-debug-ui/src/getBufferGeometry.ts
+++ 06-geometry-and-debug-ui/src/getBufferGeometry.ts
@@ -1,7 +1,7 @@
 import { BufferAttribute, BufferGeometry } from "three";
 
 export const getBufferGeometry = (...vertices: number[][]) => {
-  const positions = [].concat(...vertices);
+  const positions = new Float32Array([].concat(...vertices));
   const positionsAttribute = new BufferAttribute(positions, 3);
   const bufferGeometry = new BufferGeometry();
   bufferGeometry.setAttribute("position", positionsAttribute);
