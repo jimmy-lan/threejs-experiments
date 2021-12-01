be50cb8f9baa629b0e5976347b8aab167f4bd384
Jimmy <jimmylanbcn@gmail.com>
2021-11-30 22:25:42 -0500

    Restore experiment files

---
+++ 06-geometry-and-debug-ui/src/getRandomGeometry.ts
@@ -0,0 +1,12 @@
+import { getBufferGeometry } from "./getBufferGeometry";
+
+export const getRandomGeometry = () => {
+  const randomVertices = [];
+  for (let i = 0; i < 50 * 3; i++) {
+    randomVertices.push([
+      Math.random() - 0.5,
+      Math.random() - 0.5,
+      Math.random() - 0.5,
+    ]);
+  }
+};
