e246d2cc95c604c1989a0cfba40392ab0b07f8c6
Jimmy <jimmylanbcn@gmail.com>
2021-12-01 21:53:02 -0500

    Add 08-materials experiment

---
+++ 08-materials/src/index.ts
@@ -0,0 +1,40 @@
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import "./style.css";
+import { Size } from "./Size";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const RENDER_CANVAS_SELECTOR = "canvas.root";
+
+const size = new Size(window.innerWidth, window.innerHeight);
+
+const camera = new PerspectiveCamera(50, size.aspect);
+camera.position.z = 3;
+
+const scene = new Scene();
+scene.add(camera);
+
+const canvas = document.querySelector(RENDER_CANVAS_SELECTOR);
+const controls = new OrbitControls(camera, canvas as HTMLElement);
+controls.enableDamping = true;
+
+const renderer = new WebGLRenderer({ canvas });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 3));
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+  camera.aspect = size.aspect;
+  camera.updateProjectionMatrix();
+  renderer.setSize(size.width, size.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 3));
+});
+
+const renderFrame = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(renderFrame);
+};
+
+requestAnimationFrame(renderFrame);
