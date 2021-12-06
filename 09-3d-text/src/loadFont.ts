0307d317f111cac08fea565c7c0f0bc5f98b1fcd
Jimmy <jimmylanbcn@gmail.com>
2021-12-06 18:17:20 -0500

    Update type definition

--- 09-3d-text/src/loadFont.ts
+++ 09-3d-text/src/loadFont.ts
@@ -1,8 +1,8 @@
-import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import { Font, FontLoader } from "three/examples/jsm/loaders/FontLoader";
 
 const fontLoader = new FontLoader();
 
-export const loadFont = (url: string) => {
+export const loadFont = (url: string): Promise<Font> => {
   return new Promise((resolve, reject) => {
     return fontLoader.load(
       url,
