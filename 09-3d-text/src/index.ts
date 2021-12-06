22b6303a0204d96c6789974e1ea420fd534019c7
Jimmy <jimmylanbcn@gmail.com>
2021-12-06 18:09:46 -0500

    Load readex pro font

--- 09-3d-text/src/index.ts
+++ 09-3d-text/src/index.ts
@@ -2,19 +2,30 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
+import { FontLoader } from "three/examples/jsm/loaders/FontLoader";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  TextureLoader,
+  WebGLRenderer,
+} from "three";
 
 /**
  * Base
  */
 const gui = new GUI();
 const canvas = document.querySelector("canvas.webgl");
-const scene = new THREE.Scene();
-const textureLoader = new THREE.TextureLoader();
+const scene = new Scene();
+const textureLoader = new TextureLoader();
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/readex-pro.json", () => {
+  console.log("loaded");
+});
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
+const cube = new Mesh(new BoxGeometry(1, 1, 1), new MeshBasicMaterial());
 scene.add(cube);
 
 const size = {
@@ -33,12 +44,7 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  size.width / size.height,
-  0.1,
-  100
-);
+const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 100);
 camera.position.x = 1;
 camera.position.y = 1;
 camera.position.z = 2;
@@ -47,13 +53,11 @@ scene.add(camera);
 const controls = new OrbitControls(camera, canvas as HTMLElement);
 controls.enableDamping = true;
 
-const renderer = new THREE.WebGLRenderer({
+const renderer = new WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-const clock = new THREE.Clock();
 
 const tick = () => {
   controls.update();
