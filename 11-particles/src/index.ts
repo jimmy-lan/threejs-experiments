13b1eae059e8f047a1d064254340f553ca9c3558
Jimmy <jimmylanbcn@gmail.com>
2021-12-08 06:17:59 -0500

    Apply particle textures

--- 11-particles/src/index.ts
+++ 11-particles/src/index.ts
@@ -5,6 +5,7 @@ import {
   Points,
   PointsMaterial,
   Scene,
+  TextureLoader,
   WebGLRenderer,
 } from "three";
 import "./style.css";
@@ -17,17 +18,22 @@ const size = new Size(window.innerWidth, window.innerHeight);
 const camera = new PerspectiveCamera(50, size.aspect);
 camera.position.z = 3;
 const scene = new Scene();
+const textureLoader = new TextureLoader();
+const particleTexture = textureLoader.load("/assets/textures/particles/2.png");
 
 const numParticles = 1000;
 const positions = new Float32Array(numParticles * 3);
 for (let i = 0; i < positions.length; i++) {
   positions[i] = (Math.random() - 0.5) * 10;
 }
+
 const particlesGeometry = new BufferGeometry();
 particlesGeometry.setAttribute("position", new BufferAttribute(positions, 3));
 const particlesMaterial = new PointsMaterial({
-  size: 0.03,
+  size: 0.1,
   sizeAttenuation: true,
+  map: particleTexture,
+  color: "#ff88cc",
 });
 const particles = new Points(particlesGeometry, particlesMaterial);
 scene.add(particles);
