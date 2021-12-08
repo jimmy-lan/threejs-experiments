bdf4a3d62c4388e31e699f36dbb3a323292a7b3b
Jimmy <jimmylanbcn@gmail.com>
2021-12-07 22:01:03 -0500

    Create particles geometry and material

--- 11-particles/src/index.ts
+++ 11-particles/src/index.ts
@@ -1,4 +1,10 @@
-import { Clock, PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import {
+  PerspectiveCamera,
+  PointsMaterial,
+  Scene,
+  SphereGeometry,
+  WebGLRenderer,
+} from "three";
 import "./style.css";
 import { Size } from "./Size";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -9,6 +15,12 @@ const size = new Size(window.innerWidth, window.innerHeight);
 const camera = new PerspectiveCamera(50, size.aspect);
 camera.position.z = 3;
 const scene = new Scene();
+
+const particlesGeometry = new SphereGeometry(1, 32, 32);
+const particlesMaterial = new PointsMaterial({
+  size: 0.02,
+  sizeAttenuation: true,
+});
 
 const canvas = document.querySelector(RENDER_CANVAS_SELECTOR);
 const controls = new OrbitControls(camera, canvas as HTMLElement);
