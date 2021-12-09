3d31704e4522ed5a95e79ca19995b94df4227578
Jimmy <jimmylanbcn@gmail.com>
2021-12-09 05:53:41 -0500

    Remove random effect

--- 12-shaders/src/shaders/test/vertex.glsl
+++ 12-shaders/src/shaders/test/vertex.glsl
@@ -5,16 +5,13 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
-varying float vRandom;
-
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  // modelPosition.z = sin(modelPosition.x * 10.0) * 0.1;
-  modelPosition.z += aRandom * 0.5;
+  modelPosition.z = sin(modelPosition.x * 10.0) * 0.1;
+  // modelPosition.z += aRandom * 0.5;
+
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
-  
-  vRandom = aRandom;
 }
