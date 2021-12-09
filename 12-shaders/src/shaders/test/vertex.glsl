8558dfc224dd17ff75385ddd8e28adb96c0c55fc
Jimmy <jimmylanbcn@gmail.com>
2021-12-09 18:49:19 -0500

    Create elevation varying

--- 12-shaders/src/shaders/test/vertex.glsl
+++ 12-shaders/src/shaders/test/vertex.glsl
@@ -12,9 +12,9 @@ varying vec2 vUv;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
-  // modelPosition.z += aRandom * 0.5;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
