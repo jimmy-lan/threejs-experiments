7873736da311bb6d28ba75ff6c2ac2691a00ed7e
Jimmy <jimmylanbcn@gmail.com>
2021-12-08 19:04:42 -0500

    Adjust import order

--- 12-shaders/src/index.ts
+++ 12-shaders/src/index.ts
@@ -25,21 +25,14 @@ controls.enableDamping = true;
 const geometry = new PlaneGeometry(1, 1, 16, 16);
 const material = new RawShaderMaterial({
   vertexShader: `
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
     
-    attribute vec3 position;
-    
-    void main {
-      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
   `,
   fragmentShader: `
     precision mediump float;
     
     void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
   `,
 });
 const mesh = new Mesh(geometry, material);
