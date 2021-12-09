a6f4e27b8bcc4856c12ec7829b95f8bf46f45aeb
Jimmy <jimmylanbcn@gmail.com>
2021-12-08 20:31:58 -0500

    Comment out initial code

--- 12-shaders/src/shaders/test/vertex.glsl
+++ 12-shaders/src/shaders/test/vertex.glsl
@@ -5,5 +5,5 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 
 void main() {
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
