25db2b4e45c59bc303ae6acae4468aad48467914
Jimmy <jimmylanbcn@gmail.com>
2021-12-09 05:46:06 -0500

    Add varing

--- 12-shaders/src/shaders/test/vertex.glsl
+++ 12-shaders/src/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
+varying float vRandom;
+
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   // modelPosition.z = sin(modelPosition.x * 10.0) * 0.1;
