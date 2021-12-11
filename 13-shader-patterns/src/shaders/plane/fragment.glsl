c7955e2006d2274a286262209590599414baa179
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 17:02:00 -0500

    Comment out current pattern

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -1,7 +1,8 @@
 varying vec2 vUv;
 
 void main() {
-  float x = vUv.x;
-  // gl_FragColor = vec4(vUv, 1.0, 1.0)
-  gl_FragColor = vec4(x, x, x, 1.0);
+  // gl_FragColor = vec4(vUv, 1.0, 1.0);
+  
+  // float x = vUv.x;
+  // gl_FragColor = vec4(x, x, x, 1.0);
 }
