b5608fa5a8233a4d42c304726cea25329c650321
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 17:08:21 -0500

    Create pattern 6

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -15,7 +15,10 @@ void main() {
 
   // Pattern 5
   // If strength goes above 1, it will be treated as 1.
-  float strength = vUv.y * 12.0;
+  // float strength = vUv.y * 12.0;
+
+  // Pattern 6
+  float strength = mod(vUv.y, 0.1) * 10.0;
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);  
 
