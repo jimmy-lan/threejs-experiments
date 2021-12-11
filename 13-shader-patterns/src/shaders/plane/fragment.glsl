0d7a7a4f204c8d48835ff68b2bf8d3eaec28961f
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 17:02:48 -0500

    Title each pattern

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -1,8 +1,14 @@
 varying vec2 vUv;
 
 void main() {
+  // Pattern 1
   // gl_FragColor = vec4(vUv, 1.0, 1.0);
-  
+
+  // Pattern 2
   // float x = vUv.x;
   // gl_FragColor = vec4(x, x, x, 1.0);
+
+  // Pattern 3
+  float strength = vUv.x;
+
 }
