e6fe27d6abe48f3c21a4507e90a3aa83cabe29c4
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 16:36:35 -0500

    Update orbit controls

--- 13-shader-patterns/src/controls/orbitControls.ts
+++ 13-shader-patterns/src/controls/orbitControls.ts
@@ -1,4 +1,8 @@
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { camera } from "../objects";
+import { getCanvas } from "../helpers";
 
-const orbitControls = new OrbitControls(camera);
+const canvas = getCanvas();
+const orbitControls = new OrbitControls(camera, canvas);
+
+export { orbitControls };
