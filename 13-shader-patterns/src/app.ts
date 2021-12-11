a49837b0040dd8f8334ae16b9757cd25f888e7ee
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 16:39:15 -0500

    Implement renderFrame

--- 13-shader-patterns/src/app.ts
+++ 13-shader-patterns/src/app.ts
@@ -1,6 +1,14 @@
 import { renderer, scene, camera, plane } from "./objects";
+import { orbitControls } from "./controls/orbitControls";
+
+const renderFrame = () => {
+  orbitControls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(renderFrame);
+};
 
 export const start = () => {
   scene.add(plane);
   renderer.render(scene, camera);
+  requestAnimationFrame(renderFrame);
 };
