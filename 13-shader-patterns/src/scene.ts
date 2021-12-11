ca8a4128e1b8e9923058142de549f8635d57b377
Jimmy <jimmylanbcn@gmail.com>
2021-12-10 23:25:14 -0500

    Add docstring

--- 13-shader-patterns/src/scene.ts
+++ 13-shader-patterns/src/scene.ts
@@ -1,6 +1,10 @@
 import { PerspectiveCamera, Scene } from "three";
 import { Size } from "./models/Size";
 
+/**
+ * Create a scene with a default camera added to the scene.
+ * @param size Size of the render.
+ */
 export const createSceneWithCamera = (size: Size) => {
   const scene = new Scene();
   const camera = new PerspectiveCamera(50, size.aspect);
