ebc780bea2aaea79d6dd3d9a4de1c6d7fb1f0729
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 17:04:34 -0500

    Add pattern 4

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -8,7 +8,10 @@ void main() {
   // float strength = vUv.x;
 
   // Pattern 3
-  float strength = vUv.y;
+  // float strength = vUv.y;
+
+  // Pattern 4
+  float strength = 1.0 - vUv.y;
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);  
 
