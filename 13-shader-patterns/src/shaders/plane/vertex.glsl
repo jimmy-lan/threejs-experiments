23177f9fd15de47998d8ddb15d5b3c4734416eca
Jimmy <jimmylanbcn@gmail.com>
2021-12-11 16:28:55 -0500

    Fix vertex script

--- 13-shader-patterns/src/shaders/plane/vertex.glsl
+++ 13-shader-patterns/src/shaders/plane/vertex.glsl
@@ -1,4 +1,5 @@
 void main() {
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
   gl_Position = projectedPosition;
