8e429ae089638fc3069b0086f6190a6c59773c93
Jimmy <jimmylanbcn@gmail.com>
2021-12-12 09:31:11 -0500

    Comment out pattern 13

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -47,9 +47,9 @@ void main() {
   // float strength = step(1.1, xStrength + yStrength);
 
   // Pattern 13
-  float xBar = step(0.4, mod(vUv.x, 0.1) * 10.0) * step(0.8, mod(vUv.y, 0.1) * 10.0);
-  float yBar = step(0.8, mod(vUv.x, 0.1) * 10.0) * step(0.4, mod(vUv.y, 0.1) * 10.0);
-  float strength = xBar + yBar;
+  // float xBar = step(0.4, mod(vUv.x, 0.1) * 10.0) * step(0.8, mod(vUv.y, 0.1) * 10.0);
+  // float yBar = step(0.8, mod(vUv.x, 0.1) * 10.0) * step(0.4, mod(vUv.y, 0.1) * 10.0);
+  // float strength = xBar + yBar;
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);  
 
