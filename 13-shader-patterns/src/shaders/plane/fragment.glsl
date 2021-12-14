6e8cd7eff6a333c194c20d41c79e7a28e401e68b
Jimmy <jimmylanbcn@gmail.com>
2021-12-14 18:49:57 -0500

    Add rotate function

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -4,6 +4,13 @@ float random(vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
 }
 
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main() {
   // Pattern 1
   // gl_FragColor = vec4(vUv, 1.0, 1.0);
