fb4b17f98d3b1c383e2f5a8e6433f191e7e23925
Jimmy <jimmylanbcn@gmail.com>
2021-12-14 20:03:04 -0500

    Create pattern 49

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -287,7 +287,10 @@ void main() {
   // float strength = 1.0 - abs(cnoise(vUv * 10.0));
 
   // Pattern 48
-  float strength = sin(cnoise(vUv * 10.0) * 10.0);
+  // float strength = sin(cnoise(vUv * 10.0) * 10.0);
+
+  // Pattern 49
+  float strength = step(0.9, sin(cnoise(vUv * 10.0)* 20.0));
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);  
 }
