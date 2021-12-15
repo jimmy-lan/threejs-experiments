5617912c2857cb10df1fae3a8f08029b6eee7d76
Jimmy <jimmylanbcn@gmail.com>
2021-12-14 19:46:41 -0500

    Create pattern 42

--- 13-shader-patterns/src/shaders/plane/fragment.glsl
+++ 13-shader-patterns/src/shaders/plane/fragment.glsl
@@ -208,9 +208,16 @@ void main() {
   // float strength = angle;
 
   // Pattern 41
+  // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+  // angle /= PI * 2.0;
+  // angle += 0.5;
+  // float strength = angle;
+
+  // Pattern 42
   float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
   angle /= PI * 2.0;
-  angle += 0.5;
+  angle += 10.0;
+  angle = mod(angle, 1.0);
   float strength = angle;
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);  
